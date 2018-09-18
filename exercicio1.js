b055eec6a2fc9a19688c5527df2b296131cb5a55
Lucas Corrêa <lucashuati@hotmail.com>
2018-09-18 09:16:29 -0300

    Fix exercices

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,20 @@
-// Faça o exercício dos parágrafos aqui
-// Este arquivo ainda não está incluído no arquivo HTML
+const hasClass = function (dom, className) {
+    const classList = dom.className.split(' ');
+    return classList.find(function(element) { return element === className });
+}
+const expandeButton = function (evt) {
+    const botao = evt.currentTarget;
+    const paragrafo = botao.parentNode;
+    if(hasClass(paragrafo, 'expandido')) {
+        paragrafo.className = '';
+        botao.innerHTML = '+';
+    } else {
+        paragrafo.className = 'expandido';
+        botao.innerHTML = '-';
+    }
+}
+
+const botoes = document.querySelectorAll(".botao-expandir-retrair");
+botoes.forEach(function(botao) {
+    botao.addEventListener("click", expandeButton)
+});
--- exercicio2.js
+++ exercicio2.js
@@ -1,11 +1,25 @@
-// Faça o exercício da galeria de imagens aqui
-// Este arquivo ainda não está incluído no arquivo HTML
+const controleSlide = function(evt) {
+    const controle = evt.currentTarget;
+    if(controle.id == 'proximo') {
+        atualIndex = (atualIndex + 1) % todasAsImagens.length;
+    } else {
+        atualIndex = (atualIndex + (todasAsImagens.length - 1)) % todasAsImagens.length;
+    }
+    img.src = servidorDasImagens + todasAsImagens[atualIndex];
+}
 
+let atualIndex = 0;
+const img = document.getElementById('slide');
 const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
-  todasAsImagens = [
-    'philae-parts.jpg',
-    'philae-rosetta.jpg',
-    'philae-separation.jpg',
-    'philae-67-picture.jpg',
-    'philae-collecting.jpg'
-  ];
+    todasAsImagens = [
+        'philae-parts.jpg',
+        'philae-rosetta.jpg',
+        'philae-separation.jpg',
+        'philae-67-picture.jpg',
+        'philae-collecting.jpg'
+    ];
+
+const controles = document.querySelectorAll(".controle");
+controles.forEach(function(botao) {
+    botao.addEventListener("click", controleSlide)
+});
